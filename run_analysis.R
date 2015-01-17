b0b6227ba909dbc25d634fcf2ed394c8cdc2dfd1
henk-vdb <hvdb@telfortglasvezel.nl>
2015-01-17 20:19:28 +0100

    Get the data, log aquisition process, refined

--- run_analysis.R
+++ run_analysis.R
@@ -1,7 +1,39 @@
 
-
-dataLocation <- file.path("data", "UCI HAR Dataset")
+# Global variables
+dataDir <- "data"
+dataLocation <- file.path(dataDir, "UCI HAR Dataset")
 dataLocationAlt <- "UCI HAR Dataset"
+
+# Load the data, unzip and report into directory [dataDir]. The directory will be
+# created if non-existent.
+# Returns: the date of download.
+loadData <- function() {
+        fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+        zipFile <- file.path(dataDir, "UCI HAR Dataset.zip")
+        logFile <- file.path(dataDir, "download.log")
+        message("Downloading file from ", fileURL)
+        
+        if (!file.exists(dataDir)) dir.create(dataDir)
+        download.file(fileURL, zipFile, method = "curl", quiet = TRUE)
+        dateDownloaded <- date()
+        message("Downloaded '", zipFile, "' on ", dateDownloaded)
+        
+        result <- unzip(zipFile, exdir = dataDir)
+        nfiles <- length(result)
+        file.remove(zipFile)
+        message("Unzipped ", nfiles, " files")
+        
+        startRelPath <- nchar(dataDir) + 2
+        cat("source: ", fileURL,
+            "\ndate of download: ", dateDownloaded,
+            "\nnumber of files: ", nfiles,
+            "\nlist of files:\n", paste(substring(result, startRelPath), 
+                                        collapse = "\n"),
+            file = logFile, sep = "")
+        message("See '", logFile, "' for details")
+        
+        dateDownloaded
+}
 
 # Preferred data location is the directory "data", but we can also live with a
 # specific directory right in the working directory.
@@ -16,31 +48,4 @@ if (!file.exists(dataLocation)) {
 
 message("Using data in: ", dataLocation)
 
-# Load the data, unzip and report into directory "data", which will be created
-# if non-existent.
-loadData <- function() {
-        fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
-        zipFile <- file.path("data", "UCI HAR Dataset.zip")
-        logFile <- file.path("data", "download.log")
-        message("Downloading file from ", fileURL)
-        
-        dir.create("data")
-        download.file(fileURL, zipFile, method = "curl", quiet = TRUE)
-        dateDownloaded <- date()
-        message("Downloaded '", zipFile, "'' on ", dateDownloaded)
-        
-        result <- unzip(zipFile, exdir = "data")
-        nfiles <- length(result)
-        file.remove(zipFile)
-        message("Unzipped ", nfiles, " files in directory data/", dir("data"))
-        
-        cat("source: ", fileURL,
-            "\ndate of download: ", dateDownloaded,
-            "\nnumber of files: ", nfiles,
-            "\nlist of files:\n", paste(substring(result, 6), collapse = "\n"),
-            file = logFile, sep = "")
-        message("See '", logFile, "' for details")
-        
-        dateDownloaded
-}
 
