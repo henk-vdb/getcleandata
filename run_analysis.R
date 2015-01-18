8e11e276635f51f69126edf260c71135d72b40aa
henk-vdb <hvdb@telfortglasvezel.nl>
2015-01-18 20:14:22 +0100

    Merge data implemented

--- run_analysis.R
+++ run_analysis.R
@@ -1,9 +1,13 @@
 
+require(data.table)
+
 ################# Global variables #################
 
 dataDir <- "data"
 dataLocation <- file.path(dataDir, "UCI HAR Dataset")
 dataLocationAlt <- "UCI HAR Dataset"
+dataMerged <- file.path(dataDir, "merged_dataset")
+dataExtracted <- file.path(dataDir, "extracted_dataset")
 
 ################# Functions #################
 
@@ -38,6 +42,39 @@ loadData <- function() {
         dateDownloaded
 }
 
+# 1. Merges the training and the test sets to create one data set.
+mergeData <- function() {
+        message("Merging 'test' and 'train' data into ", dataMerged)
+        
+        if (!file.exists(dataDir)) dir.create(dataDir)
+        if (!file.exists(dataMerged)) dir.create(dataMerged)
+        
+        # copy activity_labels,txt and features.txt
+        cfiles <- c("activity_labels.txt", "features.txt")
+        from <- file.path(dataLocation, cfiles)
+        to <- file.path(dataMerged, cfiles)
+        file.copy(from, to)
+        message("Coppied ", paste(cfiles, collapse = ", "))
+        
+        mfiles <- c("subject", "X", "y")
+        for (file in mfiles) {
+                ptrain <- file.path(dataLocation, "train", paste(file, "_train.txt", sep = ""))
+                ptest <- file.path(dataLocation, "test", paste(file, "_test.txt", sep = ""))
+                pmerged <- file.path(dataMerged, paste(file, ".txt", sep = ""))
+                
+                dftrain <- read.table(ptrain)
+                dftest <- read.table(ptest)
+                dfmerged <- rbindlist(list(dftrain, dftest))
+                write.table(dfmerged, pmerged, col.names = FALSE, row.names = FALSE)
+                message("Merged '", ptrain, "' and '", ptest, "' into '", pmerged, "'")
+        }
+        # Don't need the Internal Signals, because using derived data in X.txt.
+}
+
+extractData <- function() {
+        message("Extracting data from '", dataMerged, "' into '", dataExtracted, "'")
+}
+        
 ################# Start of main script #################
 
 # Preferred data location is the directory "data", but we can also live with a
@@ -53,4 +90,12 @@ if (!file.exists(dataLocation)) {
 
 message("Using data in: ", dataLocation)
 
+# The actual assignment
+# 1. Merges the training and the test sets to create one data set.
+if (!file.exists(dataMerged)) {
+        mergeData()
+}
+
+# 2. Extracts only the measurements on the mean and standard deviation for each measurement. 
+extractData()
 
